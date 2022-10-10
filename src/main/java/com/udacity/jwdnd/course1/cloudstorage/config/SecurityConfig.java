9f84691b7674fddcaa5caf5c24e2825b25297a65
Khoi Hoang <khoi_hoang@hotmail.com>
2022-10-10 18:56:36 +0900

    Implemented a placeholder login mechanism

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -3,7 +3,14 @@ package com.udacity.jwdnd.course1.cloudstorage.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -11,16 +18,28 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(request -> request
-                        .antMatchers("/login", "/logout", "/css/*", "/js/*").permitAll()
+                        .antMatchers("/css/*", "/js/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
+                        .permitAll()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
+                        .permitAll()
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        return new InMemoryUserDetailsManager(new User("khoi", passwordEncoder.encode("khoi"), Collections.emptyList()));
     }
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SecurityController {
+@RequestMapping("/home")
+public class HomeController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping
+    public String home() {
+        return "home";
     }
 }
