fd45535e77ee24f26561add2eb0e6ba995eb1642
Khoi Hoang <khoi_hoang@hotmail.com>
2022-10-10 21:50:34 +0900

    Updated schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS USERS (
   userid INT PRIMARY KEY auto_increment,
   username VARCHAR(20) UNIQUE,
-  salt VARCHAR,
   password VARCHAR,
   firstname VARCHAR(20),
   lastname VARCHAR(20)
