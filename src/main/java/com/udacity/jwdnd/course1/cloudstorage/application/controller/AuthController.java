be1cf262ac43420564843b1829f3b2528d6dfdb5
Khoi Hoang <khoi_hoang@hotmail.com>
2022-10-12 14:39:17 +0900

    Initialize signup page

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/application/controller/AuthController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/application/controller/AuthController.java
@@ -1,10 +1,19 @@
 package com.udacity.jwdnd.course1.cloudstorage.application.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.application.form_attribute.SignUpAttribute;
+import com.udacity.jwdnd.course1.cloudstorage.domain.service.user.UserException;
+import com.udacity.jwdnd.course1.cloudstorage.domain.service.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserService userService;
 
     @GetMapping("/login")
     public String login() {
@@ -12,7 +21,24 @@ public class AuthController {
     }
 
     @GetMapping("/signup")
-    public String signup() {
+    public String viewSignup(@ModelAttribute SignUpAttribute signUpAttribute) {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signUserUp(@ModelAttribute SignUpAttribute signUpAttribute, Model model) {
+        try {
+            userService.createUser(
+                    signUpAttribute.getFirstName(),
+                    signUpAttribute.getLastName(),
+                    signUpAttribute.getUsername(),
+                    signUpAttribute.getPassword()
+            );
+        } catch (UserException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+        model.addAttribute("success", true);
+
         return "signup";
     }
 }
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/application/form_attribute/SignUpAttribute.java
@@ -0,0 +1,11 @@
+package com.udacity.jwdnd.course1.cloudstorage.application.form_attribute;
+
+import lombok.Data;
+
+@Data
+public class SignUpAttribute {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+}
