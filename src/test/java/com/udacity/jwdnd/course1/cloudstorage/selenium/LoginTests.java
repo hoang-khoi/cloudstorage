9e9bece2e88b3bb42d75901da0bc4edfd9afda63
Khoi Hoang <khoi_hoang@hotmail.com>
2022-10-12 21:13:42 +0900

    Added some login tests

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/selenium/LoginTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/selenium/LoginTests.java
@@ -1,20 +1,28 @@
 package com.udacity.jwdnd.course1.cloudstorage.selenium;
 
 import com.udacity.jwdnd.course1.cloudstorage.selenium.page_object.LoginPage;
+import com.udacity.jwdnd.course1.cloudstorage.selenium.page_object.SignUpPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginTests {
+    private static final String FIRST_NAME = "Hoang";
+    private static final String LAST_NAME = "Khoi";
+    private static final String USERNAME = "khoi";
+    private static final String PASSWORD = "12345678";
     private WebDriver driver;
     private LoginPage loginPage;
-
     @LocalServerPort
     private int port;
 
@@ -27,6 +35,10 @@ public class LoginTests {
     void setUp() {
         driver = new ChromeDriver();
         loginPage = new LoginPage(driver);
+        SignUpPage signUpPage = new SignUpPage(driver);
+
+        signUpPage.goToPage(Helper.getTestUrl(port));
+        signUpPage.signUp(FIRST_NAME, LAST_NAME, USERNAME, PASSWORD);
     }
 
     @AfterEach
@@ -39,7 +51,17 @@ public class LoginTests {
     @Test
     void testLoginPage() {
         loginPage.goToPage(Helper.getTestUrl(port));
+
         assertFalse(Helper.webElementExists(loginPage.getDivErrorMessage()));
         assertFalse(Helper.webElementExists(loginPage.getDivLoggedOutMessage()));
+    }
+
+    @Test
+    void testLoginPage_Login_Success() {
+        loginPage.goToPage(Helper.getTestUrl(port));
+        loginPage.login(USERNAME, PASSWORD);
+
+        assertEquals(Helper.getTestUrl(port) + "/", driver.getCurrentUrl());
+        assertEquals("Home", driver.getTitle());
     }
 }
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/selenium/page_object/LoginPage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/selenium/page_object/LoginPage.java
@@ -16,9 +16,24 @@ public class LoginPage implements PageObject {
     @FindBy(id = "error-msg")
     private WebElement divLoggedOutMessage;
 
+    @FindBy(id = "inputUsername")
+    private WebElement inputUsername;
+
+    @FindBy(id = "inputPassword")
+    private WebElement inputPassword;
+
+    @FindBy(id = "login-button")
+    private WebElement buttonSubmit;
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password) {
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        buttonSubmit.submit();
     }
 
     @Override
