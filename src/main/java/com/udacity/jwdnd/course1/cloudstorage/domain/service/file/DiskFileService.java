05211556bffadf47fca92f4f14e1e9bd25a5036c
Khoi Hoang <khoi_hoang@hotmail.com>
2022-10-13 18:38:00 +0900

    Finalized FileService

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/domain/service/file/DiskFileService.java
@@ -0,0 +1,45 @@
+package com.udacity.jwdnd.course1.cloudstorage.domain.service.file;
+
+import com.udacity.jwdnd.course1.cloudstorage.domain.repository.file.FileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class DiskFileService implements FileService {
+    private final FileRepository fileRepository;
+
+    @Override
+    public void upload(MultipartFile multipartFile) throws FileServiceException {
+        try {
+            fileRepository.save(multipartFile);
+        } catch (IOException e) {
+            throw new FileServiceException("Failed to save the file.", e);
+        }
+    }
+
+    @Override
+    public void delete(String filename) {
+        fileRepository.delete(filename);
+    }
+
+    @Override
+    public List<String> listAllFilenames(String filename) {
+        return fileRepository.listAll();
+    }
+
+    @Override
+    public File getFileByName(String name) throws FileServiceException {
+        File file = fileRepository.getFileByName(name);
+        if (!file.exists()) {
+            throw new FileServiceException("File not found.", null);
+        }
+
+        return file;
+    }
+}
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/domain/service/file/FileServiceException.java
@@ -0,0 +1,7 @@
+package com.udacity.jwdnd.course1.cloudstorage.domain.service.file;
+
+public class FileServiceException extends Exception {
+    public FileServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
