90da91c3893f66d523bb328d48c46d1ca14284b0
Khoi Hoang <khoi_hoang@hotmail.com>
2022-10-14 20:53:33 +0900

    Deleted faulty implementation of FileService

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/domain/service/file/FileServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.udacity.jwdnd.course1.cloudstorage.domain.service.file;
-
-public class FileServiceException extends Exception {
-    public FileServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
